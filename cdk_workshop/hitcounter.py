7f1743c77129dbfa3d928926abeb67e43bc467e0
tysmmsy <tysmmsy@gmail.com>
2023-04-24 15:06:53 +0900

    modify typo

--- cdk_workshop/hitcounter.py
+++ cdk_workshop/hitcounter.py
@@ -21,7 +21,7 @@ class HitCounter(Construct):
             self, 'HitCounterHandler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             handler='hitcounter.handler',
-            code=_lambda.Code.asset('lambda'),
+            code=_lambda.Code.from_asset('lambda'),
             environment={
                 'DOWNSTREAM_FUNCTION_NAME': downstream.function_name,
                 'HITS_TABLE_NAME': table.table_name,
